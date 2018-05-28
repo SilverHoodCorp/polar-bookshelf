bedf5ce04a58d386b0d5e4fa30c7ad8a2cd0d43d
burton <burton@spinn3r.com>
2018-05-28 11:54:47 -0700

    init and using pdf.js 2.0

---
+++ README.md
@@ -0,0 +1,36 @@
+<img src="https://github.com/praharshjain/Electron-PDF-Viewer/blob/master/icon.ico" width="40">&nbsp;&nbsp;Electron-PDF-Viewer
+==========  
+
+PDF viewer created using [Electron framework](https://electron.atom.io) and [PDF.js](https://mozilla.github.io/pdf.js)  
+
+Usage
+----------------
+
+Download executable jar from [Releases](https://github.com/praharshjain/Electron-PDF-Viewer/releases)
+
+Or build from source :
+
+Install dependencies (Node.js, npm etc) -
+```
+$ sudo apt-get install python-software-properties
+$ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+$ sudo apt-get update
+$ sudo apt-get install build-essential
+$ sudo apt-get install nodejs
+$ sudo apt-get install npm
+```
+To run -
+```
+$ git clone https://github.com/praharshjain/Electron-PDF-Viewer.git
+$ cd Electron-PDF-Viewer
+$ npm install && npm start
+```
+Screenshot
+----------------  
+<img src="https://github.com/praharshjain/Electron-PDF-Viewer/blob/master/screenshot.png" width="1200">
+  
+License
+----------------
+[PDF.js](https://github.com/mozilla/pdf.js) is available under  Apache License.  
+[Electron](https://github.com/electron/electron) is released under MIT License.  
+Rest of the code is MIT licensed.
---
+++ main.js
@@ -0,0 +1,218 @@
+const electron = require('electron');
+const fspath = require('path');
+const url = require('url');
+const app = electron.app;
+const shell = electron.shell;
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+const dialog = electron.dialog;
+const ipcMain = electron.ipcMain;
+const crashReporter = electron.crashReporter;
+const BrowserWindow = electron.BrowserWindow;
+const nativeImage = require('electron').nativeImage;
+const options = { extraHeaders: 'pragma: no-cache\n' }
+const app_icon = nativeImage.createFromPath(fspath.join(__dirname, 'icon.ico'));
+let mainWindow, splashwindow;
+var contextMenu = null;
+var filepath = null;
+var quitapp, URL;
+
+function sleep(millis) {
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while (curDate - date < millis);
+}
+crashReporter.start({ productName: 'Polar eBook Reader', companyName: 'Praharsh', submitURL: 'https://praharsh.xyz/projects/PDFViewer/crash', autoSubmit: false });
+//creating menus for menu bar
+const template = [{
+        label: 'File',
+        submenu: [{
+                label: 'Open',
+                accelerator: 'CmdOrCtrl+O',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow) {
+                        dialog.showOpenDialog({
+                            filters: [
+                                { name: 'PDF', extensions: ['pdf'] }
+                            ],
+                            properties: ['openFile']
+                        }, function(path) {
+                            if (path) {
+                                filepath = path;
+                                if (path.constructor === Array)
+                                    path = path[0];
+                                mainWindow.loadURL('file://' + __dirname + '/pdfviewer/web/viewer.html?file=' + encodeURIComponent(path), options);
+                            }
+                        });
+                    }
+                }
+            },
+            {
+                label: 'Open Containing Folder',
+                accelerator: 'CmdOrCtrl+F',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow && filepath)
+                        shell.showItemInFolder("file:///" + filepath);
+                }
+            },
+            {
+                label: 'Print',
+                accelerator: 'CmdOrCtrl+P',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow) focusedWindow.webContents.print();
+                }
+            },
+            {
+                label: 'Close',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow) focusedWindow.loadURL('file://' + __dirname + '/default.html', options);
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Exit',
+                accelerator: 'Alt+F4',
+                role: 'close'
+            },
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
+            { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
+            { type: 'separator' },
+            { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
+            { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [{
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.webContents.reloadIgnoringCache();
+                }
+            },
+            {
+                label: 'Toggle Full Screen',
+                accelerator: (function() {
+                    if (process.platform == 'darwin')
+                        return 'Ctrl+Command+F';
+                    else
+                        return 'F11';
+                })(),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            },
+        ]
+    },
+    {
+        label: 'Window',
+        role: 'window',
+        submenu: [
+            { label: 'Minimize', accelerator: 'CmdOrCtrl+M', role: 'minimize' },
+            { label: 'Close', accelerator: 'CmdOrCtrl+W', role: 'close' },
+        ]
+    },
+    {
+        label: 'Help',
+        role: 'help',
+        submenu: [{
+                label: 'About',
+                click: function() {
+                    dialog.showMessageBox(mainWindow, {
+                        type: 'info',
+                        buttons: ['OK'],
+                        title: 'Polar eBook Reader 1.0',
+                        message: 'Version 1.0',
+                        detail: 'Created By - Praharsh Jain',
+                        icon: app_icon
+                    });
+                }
+            },
+            { label: 'Learn More', click: function() { shell.openExternal('https://github.com/praharshjain/Electron-PDF-Viewer'); } },
+        ]
+    },
+];
+var menu = Menu.buildFromTemplate(template);
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
+});
+if (shouldQuit) { app.quit(); return; }
+app.on('ready', function() {
+    splashwindow = new BrowserWindow({ width: 400, height: 300, center: true, resizable: false, movable: false, alwaysOnTop: true, skipTaskbar: true, frame: false });
+    splashwindow.loadURL('file://' + __dirname + '/splash.html');
+    contextMenu = Menu.buildFromTemplate([
+        { label: 'Minimize', type: 'radio', role: 'minimize' },
+        { type: 'separator' },
+        { label: 'Exit', type: 'radio', role: 'close' },
+    ]);
+    //for OS-X
+    if (app.dock) {
+        app.dock.setIcon(app_icon);
+        app.dock.setMenu(contextMenu);
+    }
+    Menu.setApplicationMenu(menu);
+    const appIcon = new Tray(app_icon);
+    appIcon.setToolTip('PDF Viewer');
+    appIcon.setContextMenu(contextMenu);
+    //splash screen for 3 seconds
+    setTimeout(createWindow, 3000);
+});
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') { app.quit(); }
+});
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) { createWindow(); }
+});
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ minWidth: 400, minHeight: 300, width: 800, height: 600, show: false, icon: app_icon, webPreferences: { nodeIntegration: false, defaultEncoding: 'UTF-8' } });
+    mainWindow.on('close', function(e) {
+        e.preventDefault();
+        mainWindow.webContents.clearHistory();
+        mainWindow.webContents.session.clearCache(function() {
+            mainWindow.destroy();
+        });
+    });
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+        app.quit();
+    });
+    mainWindow.webContents.on('new-window', function(e, url) {
+        e.preventDefault();
+        shell.openExternal(url);
+    });
+    mainWindow.webContents.on('devtools-opened', function(e) {
+        e.preventDefault();
+        this.closeDevTools();
+    });
+    mainWindow.webContents.on('will-navigate', function(e, url) {
+        e.preventDefault();
+        shell.openExternal(url);
+    });
+    mainWindow.loadURL('file://' + __dirname + '/default.html', options);
+    mainWindow.once('ready-to-show', () => {
+        splashwindow.close();
+        splashwindow = null;
+        mainWindow.maximize();
+        mainWindow.show();
+    });
+}
