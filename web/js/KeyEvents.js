5abba02fed37dce428ca249abe8e260f19cc93ff
Kevin Burton <burtonator+github.com@gmail.com>
2018-06-05 13:54:26 -0700

    most of the main functionality works again

---
+++ web/js/KeyEvents.js
@@ -0,0 +1,18 @@
+export class KeyEvents {
+
+    /**
+     * Return true if the 'meta' keys are active. 
+     */
+    static isKeyMetaActive(event) {
+        if (KeyEvents.isMacOS()) {
+            return event.altKey && event.metaKey;
+        } else {
+            return event.ctrlKey && event.altKey;
+        }
+    }
+
+    static isMacOS() {
+        return navigator.platform === "MacIntel";
+    }
+
+}
