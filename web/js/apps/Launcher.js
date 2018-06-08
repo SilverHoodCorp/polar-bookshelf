e04dc4ed460d325cc42410ca3182fd3677cab046
burton <burton@spinn3r.com>
2018-06-07 18:36:10 -0700

    clean up of launcher

--- web/js/apps/Launcher.js
+++ web/js/apps/Launcher.js
@@ -1,3 +1,8 @@
+const {SystemClock} = require("../time/SystemClock.js");
+const {Model} = require("../model.js");
+const {WebController} = require("../controller/WebController.js");
+const {WebView} = require("../view/WebView.js");
+const {TextHighlightView} = require("../highlights/text/view/TextHighlightView");
 
 /**
  * Basic class for connecting event listeners and then running a launchFunction
@@ -10,34 +15,38 @@ module.exports.Launcher = class {
     /**
      * Launch the app with the given launch function.
      *
-     * @param launchFunction
+     * @param persistenceLayerFactory
      */
-    constructor(launchFunction) {
-        this.launchFunction = launchFunction;
-    }
-
-
-    async start(persistenceLayer, controller, mode) {
-
-        await persistenceLayer.init();
-
-        controller.start();
-        console.log("Controller started in mode: " + mode);
-
+    constructor(persistenceLayerFactory) {
+        this.persistenceLayerFactory = persistenceLayerFactory;
     }
 
     /**
      * Trigger the launch function.
      */
-    trigger() {
-        this.launchFunction(this);
+    async trigger() {
+
+        let persistenceLayer = await this.persistenceLayerFactory();
+
+        let clock = new SystemClock();
+        let model = new Model(persistenceLayer, clock);
+        let controller = new WebController(model);
+        let view = new WebView(model);
+
+        view.start();
+        new TextHighlightView(model).start();
+
+        await persistenceLayer.init();
+
+        controller.start();
+
     }
 
-    launch() {
+    async launch() {
 
         if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
             console.log("Already completed loading.");
-            this.trigger();
+            await this.trigger();
         } else {
             console.log("Waiting for DOM content to load");
             document.addEventListener('DOMContentLoaded', this.trigger.bind(this), true);
--- web/js/apps/chrome.js
+++ web/js/apps/chrome.js
@@ -3,28 +3,20 @@ const {DocMeta} = require("../metadata/DocMeta");
 const {DocMetas} = require("../metadata/DocMetas");
 const {DocInfo} = require("../metadata/DocInfo");
 const {Controller} = require("../controller/Controller.js");
-const {WebController} = require("../controller/WebController.js");
-const {WebView} = require("../view/WebView.js");
 
-const {SystemClock} = require("../time/SystemClock.js");
 const {PersistenceLayer} = require("../datastore/PersistenceLayer.js");
 const {MemoryDatastore} = require("../datastore/MemoryDatastore.js");
-const {Model} = require("../model.js");
 const {Electron} = require("../Electron");
 const {Launcher} = require("./Launcher");
-const {TextHighlightView} = require("../highlights/text/view/TextHighlightView");
 
-async function launchDev(launcher) {
+async function persistenceLayerFactory() {
 
     console.log("Launching in dev mode.");
 
     let datastore = new MemoryDatastore();
-
     let persistenceLayer = new PersistenceLayer(datastore);
-    let clock = new SystemClock();
-    let model = new Model(persistenceLayer, clock);
-    let controller = new WebController(model);
-    let view = new WebView(model);
+
+    await persistenceLayer.init();
 
     // create some fake documents for our example PDFs
     let fingerprint = "110dd61fd57444010b1ab5ff38782f0f";
@@ -33,11 +25,10 @@ async function launchDev(launcher) {
     DocMetas.addPagemarks(docMeta, {nrPages: 1, offsetPage: 4, percentage: 50})
     await persistenceLayer.sync(fingerprint, docMeta);
 
-    view.start();
-    new TextHighlightView(model).start();
-
-    await launcher.start(datastore, controller, "dev");
+    return persistenceLayer;
 
 }
 
-new Launcher(launchDev).launch();
+new Launcher(persistenceLayerFactory).launch().then(function () {
+    console.log("App now loaded.");
+});
