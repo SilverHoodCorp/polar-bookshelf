272ba4cb809ef727d8b2e58be5be417bbe37c5ac
burton <burton@spinn3r.com>
2018-06-10 15:38:24 -0700

    packages aren't working yet

--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,17 @@ module.exports = [
             publicPath: '/web/js/apps'
         }
 
+    },
+    {
+        mode: 'development',
+        entry: {
+            injector: './web/js/apps/injector.js',
+        },
+        output: {
+            path: path.resolve(__dirname, 'web/js/apps'),
+            filename: '[name]-bundle.js',
+            publicPath: '/web/js/apps'
+        }
     }
+
 ];
